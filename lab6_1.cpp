0d18a65804e132a01de949e2844b1dcc20a37a88
Smart309 <pattharapon_takham@cmu.ac.th>
2021-11-26 15:50:54 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int N = 1 ;
+    int Even = 0 ;
+    int Odd = 0 ;
+    while (N != 0)
+    {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N ;
+    if (N%2 == 0)
+    {
+        Even += 1 ;
+    }else {
+        Odd += 1 ;
+    }
+    
+    }
+    
+    cout << "#Even numbers = " << Even-1 ;
+    cout << "\n#Odd numbers = " << Odd ;
     return 0;
 }
